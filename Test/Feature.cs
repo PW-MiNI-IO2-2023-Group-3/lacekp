ff25794b82e390cea91a6d857c1d8a6042f8375c
Paweł Łacek <01161401@pw.edu.pl>
2023-03-06 08:56:56 +0100

    Add project files.

---
+++ Test/Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Feature
+    {
+        public static string Method(string name)
+        {
+            if (name == "pawel")
+            {
+                return "ok";
+            }
+
+            return "bad";
+        }
+    }
+}
